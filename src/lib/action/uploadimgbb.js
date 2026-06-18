07c6e345dd5e19b1949e634c60d40cc1c3e4551f
jahidhasan909 <jjjjahidhasan999@gmail.com>
2026-06-19 00:03:20 +0600

    better and mongodb  install and configuration done

---
+++ src/lib/action/uploadimgbb.js
@@ -0,0 +1,15 @@
+
+
+
+
+export const uploadImagebb = async (image) => {
+    const formdata = new FormData()
+    formdata.append('image', image)
+    const res = await fetch(`https://api.imgbb.com/1/upload?key=${process.env.NEXT_PUBLIC_IMGBB_API}`, {
+        method: 'POST',
+        body: formdata
+    })
+
+    const data = res.json()
+    return data.data;
+}
