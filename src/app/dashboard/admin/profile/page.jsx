1a301129cb10fe917823d685aca0dd68dc7cf788
jahidhasan909 <jjjjahidhasan999@gmail.com>
2026-06-25 15:31:55 +0600

    admin side dashboard profile patch edit profile api call and configuration successfully done

--- src/app/dashboard/admin/profile/page.jsx
+++ src/app/dashboard/admin/profile/page.jsx
@@ -1,10 +1,32 @@
-import AdminProfile from '@/Components/dashboardComponents/AdminComponents/Adminprofile';
+
+import ProfileAdmin from '@/Components/dashboardComponents/AdminComponents/Profile';
+import { auth } from '@/lib/auth';
+import { headers } from 'next/headers';
 import React from 'react';
 
-const AdminProfilepage = () => {
+const AdminProfilepage = async () => {
+
+
+    const session = await auth.api.getSession({
+        headers: await headers()
+    })
+    const user = session?.user
+
+
+    const baseurl = process.env.NEXT_PUBLIC_BASE_URL
+
+    const res = await fetch(`${baseurl}/api/own/users?email=${user?.email}`)
+
+    const userData = await res.json()
+    
+
+
+
+
+
     return (
         <div>
-            <AdminProfile></AdminProfile>
+            <ProfileAdmin userData={userData}></ProfileAdmin>
         </div>
     );
 };
