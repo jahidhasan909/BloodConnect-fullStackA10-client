741124244f84441b5b3df4951f2b50f19a53468e
jahidhasan909 <jjjjahidhasan999@gmail.com>
2026-06-27 02:20:30 +0600

    add new dynamic get api fetch active donors marquee animation

---
+++ src/Components/DonorMarquee/DonorMarquee.jsx
@@ -0,0 +1,53 @@
+'use client'
+import React, { useEffect, useState } from 'react';
+import Marquee from 'react-fast-marquee';
+
+const DonorMarquee = () => {
+    const baseurl = process.env.NEXT_PUBLIC_BASE_URL
+    const [activeDonors, setActiveDonors] = useState([])
+
+    useEffect(() => {
+        fetch(`${baseurl}/api/users`)
+            .then(res => res.json())
+            .then(data => {
+                
+                const filtered = data.filter(user =>
+                    user.status === 'active' && user.role === 'donor'
+                );
+                setActiveDonors(filtered);
+            })
+            .catch(err => console.error("Error fetching donors:", err));
+    }, [baseurl]);
+
+    return (
+        <div className="w-full py-2 bg-red-50 ">
+            <Marquee className=' grayscale  container mx-auto hover:grayscale-0' speed={80} pauseOnHover={true} gradient={true} gradientColor={[255, 255, 255]}>
+                {activeDonors.map((donor) => (
+                    <div
+                        key={donor._id}
+                        className="flex relative overflow-hidden items-center gap-1  mx-3 max-w-[250px]  p-4 bg-slate-50 border border-slate-200 rounded-2xl shadow-sm hover:shadow-md transition-shadow"
+                    >
+                        
+                        <img
+                            src={donor.image}
+                            alt={donor.name}
+                            className="w-12 h-12 rounded-full object-cover border-2 border-red-500"
+                        />
+
+                     
+                        <div className="flex flex-col pr-13">
+                            <h3 className="font-bold text-gray-800">{donor.name}</h3>
+                            <p className="text-sm text-gray-500 truncate">{donor.upazila}, {donor.district}</p>
+                        </div>
+
+                        <div className="absolute  right-0  top-0  bg-red-600 text-white px-4 py-1.5 rounded-bl-2xl font-black text-sm tracking-wide shadow-xs">
+                            {donor?.bloodGroup}
+                        </div>
+                    </div>
+                ))}
+            </Marquee>
+        </div>
+    );
+};
+
+export default DonorMarquee;
--- src/app/page.js
+++ src/app/page.js
@@ -1,13 +1,15 @@
 import HeroBanner from "@/Components/Banner/Banner";
 import CommunityImpactStats from "@/Components/CommunityImpact/CommunityImpact";
 import ContactSection from "@/Components/ContactUs/Contactus";
+import DonorMarquee from "@/Components/DonorMarquee/DonorMarquee";
 import FeaturedSection from "@/Components/Featuredsection/Featuredesction";
-import Image from "next/image";
 
-export default function Home() {
+
+export  default function Home() {
   return (
     <>
     <HeroBanner></HeroBanner>
+    <DonorMarquee></DonorMarquee>
     <CommunityImpactStats></CommunityImpactStats>
     <FeaturedSection></FeaturedSection>
     <ContactSection></ContactSection>
